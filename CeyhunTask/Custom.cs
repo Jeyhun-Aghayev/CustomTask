4a20b764159a362e9532be8b74c13135206c961b
Jeyhun-Aghayev <145680779+Jeyhun-Aghayev@users.noreply.github.com>
2023-10-11 13:34:33 +0400

    Add files via upload

---
+++ CeyhunTask/Custom.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ceyhun
+{
+    internal class Custom
+    {
+        public string Name;
+        public bool ContainMethod(char SearchChar)
+        {
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if (SearchChar == Name[i])
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        public string SubStringElement(int start)
+        {
+            StringBuilder newString = new StringBuilder();
+
+            for (int i = start; i < Name.Length; i++)
+            {
+                newString.Append(Name[i]);
+            }
+            return newString.ToString();
+        }
+        public string ReplaceChar(char ChangeChar, char GivenChar)
+        {
+            StringBuilder newString = new StringBuilder();
+            for (int i = 0; i < Name.Length; i++)
+            {
+
+                if (ChangeChar == Name[i])
+                {
+                    newString.Append(GivenChar);
+                }
+                else
+                {
+                    newString.Append(Name[i]);
+                }
+            }
+            return newString.ToString();
+        }
+
+        public string Trim_Remove_Space()
+        {
+            StringBuilder newString = new StringBuilder();
+            int startIndex = 0;
+            int endIndex = - 1;
+
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if (Name[i] != ' ')
+                {
+                    startIndex = i;
+                    break;
+                }
+            }
+            for (int i = Name.Length - 1; i >= 0; i--)
+            {
+                if (Name[i] != ' ')
+                {
+                    endIndex = i;
+                    break;
+                }
+            }
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                newString.Append(Name[i]);
+            }
+            return newString.ToString();
+
+
+
+        }
+    }
+}
---
+++ CeyhunTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ceyhun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Person person = new Person("    ceyhun Aghayev   ",19,"912983178523");
+            Console.WriteLine($"{person.FullNamee} {person.Age} {person.PhoneNumber}");*/
+            
+            Custom custom = new Custom();
+            custom.Name = "   Tedfkgjndcset   ";
+
+
+            /*Console.WriteLine(custom.ContainMethod('e'));*/
+
+            /*Console.WriteLine(custom.SubStringElement(3));*/
+
+            /*Console.WriteLine(custom.ReplaceChar('e', 'a'));*/
+
+            Console.WriteLine(custom.Trim_Remove_Space());
+
+
+
+
+        }
+    }
+}
